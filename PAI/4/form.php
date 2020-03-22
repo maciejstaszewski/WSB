822b3ea66bd1e04d6b26d263bd318230f8e947e9
maciejstaszewski <mcsta@o2.pl>
2020-03-22 20:30:28 +0100

    Z4-poprawione

--- PAI/4/form.php
+++ PAI/4/form.php
@@ -7,12 +7,22 @@
 </head>
 <body>
     <h3>Formularz rejestracji</h3>
-    <form method="post" action="./script/4_form.php">
-        <input type="text" name="name" placeholder="Imię"><br>
+    <?php
+        if(isset($_GET['errorPass'])) {
+            echo "Podane hasła są różne<br>";
+        }
+        if (isset($_GET['emptyData'])) {
+            echo "Wypełnij wszystkie dane<br>";
+        }
+    ?>
+    <form method="post" action="./scripts/4_form.php">
+        <input type="text" name="name" placeholder="Imię" autofocus autocomplete="off"><br>
         <input type="text" name="surname" placeholder="Nazwisko"><br>
         <input type="password" name="pass1" placeholder="Hasło"><br>
-        <input type="password" name="pass2" placeholder="Podaj ponownie"><br>
-        <input type="submit" value="wyslij formularz">
+        <input type="password" name="pass2" placeholder="Podaj ponownie hasło"><br>
+        <input type="radio" name="gender" value="k" checked>Kobieta<br>
+        <input type="radio" name="gender" value="m">Mężczyzna<br>
+        <input type="submit" value="Wyslij formularz">
     </form> 
 </body>
 </html>
